b6db032de7fad95c8f7b9b7cf89ea1d5a688bed1
ianmenethil <62359909+ianmenethil@users.noreply.github.com>
2024-06-07 14:38:58 +1000

    Deploy website - based on a064198427fc13156b532b5d5aed1e72ee33b6ae

--- src/components/APIs/SwaggerUIComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import SwaggerUI from 'swagger-ui-react';
-import 'swagger-ui-react/swagger-ui.css';
-import APIs from '/src/components/APIs/APIs.json';
-const SwaggerUIComponent = () => {
-    return <SwaggerUI spec={APIs} />;
-};
-
-export default SwaggerUIComponent;
